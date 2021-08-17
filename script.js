e265544eb60fcd9cd6d44ad89b1cab8a5402315b
hikedan <89092386+hikedan@users.noreply.github.com>
2021-08-18 01:01:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+class Holomen {
+    constructor(name) {
+        this.name = name
+
+    }
+}
+
+class Game {
+    constructor() {
+        
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.querySelector("#intro").innerHTML="Hello!";
+})
